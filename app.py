947b1755bf82b07ac08afc64f57a9283f3e21a4a
willbigger <wmb8yt@virginia.edu>
2022-11-27 17:47:14 -0500

    more options (close to final) and better format

--- app.py
+++ app.py
@@ -15,11 +15,19 @@ def pretty_print():
     # need to add user deletion (maybe require username AND password)
     matrix=[
         ["Choose a number  ", "", "", "", ""],
-        ["0: Exit  ", "1: View All User Profiles  ", "2: View All Movies  ", "3: View All Reviews  ", "4: View All Genres  "], 
-        ["5: Add a New Genre  ", "6: View All Actors  ", "7: Change/Update Favorite Actor  ", "8: Change/Update Favorite Actor  ", "9: Search for MovieID  "], 
-        ["10: View All Directors  ", "11: View All Authors and their reviews  ", "12: View All Current Favorite Genres  ", "13: Add Favorite Genre  ", "14: Delete Favorite Genre  "],
-        ["15: View All Current Favorite Movies  ", "16: Add Favorite Movie  ", "17: Delete Favorite Movie  ", "", ""],
-        ["20: View All Reviews  ", "21: Add a Review  ", "22: Delete a Review  ", "23: Update a Review  ", "24: Like a Review  "], 
+        ["", "", "", "", ""],
+        ["0: Exit  ", "", "", "", ""], 
+        ["1: View All User Profiles  ", "2: View All Movies  ", "3: View All Genres  ", "4: View All Actors  ", "5: View All Directors  "], 
+        ["6: Add a New Genre  ", "7: Search for MovieID  ", "8: View Movies with Their Genres  ",  "", ""], 
+        ["", "", "", "", ""],
+        ["Favorites  ", "", "", "", ""],
+        ["11: Change/Update Favorite Actor  ", "12: Change/Update Favorite Actor  ", "", "", ""],
+        ["16: View All Current Favorite Genres  ", "17: Add Favorite Genre  ", "18: Delete Favorite Genre  ", "", ""],
+        ["21: View All Current Favorite Movies  ", "22: Add Favorite Movie  ", "23: Delete Favorite Movie  ", "", ""],
+        ["", "", "", "", ""],
+        ["Reviews  ", "", "", "", ""],
+        ["26: View All Authors and their reviews  ", "", "", "", ""], 
+        ["31: View All Reviews  ", "32: Add a Review  ", "33: Delete a Review  ", "34: Update a Review  ", "35: Like a Review  "], 
     ]
 
     s = [[str(e) for e in row] for row in matrix]
@@ -42,8 +50,6 @@ if trys == 0 and not logged_in:
 else:
     current_user_email = main_list[1]
     user_input_number = -1
-    print("\n\n\n\n\n\n\n\n\n\n----------------------------------------------------------------------")
-    print("\nCurrent user email: " + current_user_email + "\n")
 
 while user_input_number != 0:
     if user_input_number == 1:
@@ -51,46 +57,55 @@ while user_input_number != 0:
     elif user_input_number == 2:
         movie.ViewMovies()
     elif user_input_number == 3:
-        review.viewReviews()
-    elif user_input_number == 4:
         genre.viewGenres()
-    elif user_input_number == 5:
-        genre.addGenre()
-    elif user_input_number == 6:
+    elif user_input_number == 4:
         movie_actor.viewActors()
-    elif user_input_number == 7:
-        user_profile.updateFavoriteActor(current_user_email)
-    elif user_input_number == 8:
-        user_profile.updateFavoriteDirector(current_user_email)
-    elif user_input_number == 9:
-        movie.searchMovies()
-    elif user_input_number == 10:
+    elif user_input_number == 5:
         directs_movie.viewDirectors()
-    elif user_input_number == 11:
-        authored_by.viewAuthors()
-    elif user_input_number == 12:
-        favorite_genre.viewFavoriteGenres(current_user_email)
-    elif user_input_number == 13:
-        favorite_genre.addFavoriteGenres(current_user_email)
-    elif user_input_number == 14:
-        favorite_genre.deleteFavoriteGenres(current_user_email)
-    elif user_input_number == 15:
-        favorite_movie.viewFavoriteMovies(current_user_email)
-    elif user_input_number == 16:
-        favorite_movie.addFavoriteMovies(current_user_email)
-    elif user_input_number == 17:
-        favorite_movie.deleteFavoriteMovie(current_user_email)
-    elif user_input_number == 20:
-        review.viewReviews()
-    elif user_input_number == 21:
-        review.addReview(current_user_email)
-    elif user_input_number == 22:
-        review.deleteReview(current_user_email)
-    elif user_input_number == 23:
-        review.updateReview()
-    elif user_input_number == 24:
-        review.likeReview()
 
+    elif user_input_number == 6:
+        genre.addGenre()
+    elif user_input_number == 7:
+        movie.searchMovies()
+    elif user_input_number == 8:
+        movie_genre.viewMovieGenres()
+    
+
+    elif user_input_number == 11:
+        user_profile.updateFavoriteActor(current_user_email)
+    elif user_input_number == 12:
+        user_profile.updateFavoriteDirector(current_user_email)
+
+    elif user_input_number == 16:
+        favorite_genre.viewFavoriteGenres(current_user_email)
+    elif user_input_number == 17:
+        favorite_genre.addFavoriteGenres(current_user_email)
+    elif user_input_number == 18:
+        favorite_genre.deleteFavoriteGenres(current_user_email)
+
+    elif user_input_number == 21:
+        favorite_movie.viewFavoriteMovies(current_user_email)
+    elif user_input_number == 22:
+        favorite_movie.addFavoriteMovies(current_user_email)
+    elif user_input_number == 23:
+        favorite_movie.deleteFavoriteMovie(current_user_email)
+
+    elif user_input_number == 26:
+        authored_by.viewAuthors()
+
+    elif user_input_number == 31:
+        review.viewReviews()
+    elif user_input_number == 32:
+        review.addReview(current_user_email)
+    elif user_input_number == 33:
+        review.deleteReview(current_user_email)
+    elif user_input_number == 34:
+        review.updateReview()
+    elif user_input_number == 35:
+        review.likeReview()
+    print("----------------------------------------------------------------------")
+    print("\n\n\n\n\n\n\n\n\n\n----------------------------------------------------------------------")
+    print("\nCurrent user email: " + current_user_email + "\n")
     pretty_print()
     user_input_number = input("Enter a number: ")
     if len(user_input_number) == 0 or not user_input_number.isdigit():
@@ -99,4 +114,5 @@ while user_input_number != 0:
     else:
         user_input_number = int(user_input_number)
 
+print("----------------------------------------------------------------------")
 print("\nAll changes saved\nBye!")
